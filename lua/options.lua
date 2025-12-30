32cc7f5215677fe00fef7361545dea48bdf507ee
maadi <maadiazizii@gmail.com>
2025-12-30 19:09:38 +0330

    Fix golang formatting problem.

--- lua/options.lua
+++ lua/options.lua
@@ -203,11 +203,3 @@ require('telescope').setup{
     },
   }
 }
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-  end,
-})
